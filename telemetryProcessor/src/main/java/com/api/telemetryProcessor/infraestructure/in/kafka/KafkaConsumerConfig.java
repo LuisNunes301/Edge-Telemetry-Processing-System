e9f849d3a7a0f8d365d38e5a418ffd2a508e1dc6
LuisNunes301 <vnunes30@gmail.com>
2026-09-15 04:36:06 -0300

    feat(resilience): add Kafka Dead Letter Queue (DLQ) and error recovery handling

---
+++ telemetryProcessor/src/main/java/com/api/telemetryProcessor/infraestructure/in/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,27 @@
+package com.api.telemetryProcessor.infraestructure.in.kafka;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaOperations;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+
+    @Bean
+    public DefaultErrorhandler errorHandler(KafkaOperations
+    <Object,Object> tempate){
+        // Vai tentar processar 3 vezes, aguardando 2 segundos a cada tentativa
+        FixedBackOff fixedBackOff = new FixedBackOff(200L,3L)
+        // Configura o recoverer para enviar a mensagem falha para o tópico
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(template,
+                (r, e) -> new TopicPartition(r.topic() + ".DLT", r.partition()));
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(recoverer, fixedBackOff);
+
+        errorHandler.addNotRetryableExceptions(IllegalArgumentException.class);
+        return errorHandler;
+    }
+}
